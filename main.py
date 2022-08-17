0b8a2f80ab93bdc16b4a0f3b4bc0cf6585434162
ISDL <ISDL@ISDL-Public-iMac.local>
2022-08-17 17:00:59 +0900

    first commit

--- main.py
+++ main.py
@@ -42,7 +42,13 @@ if __name__ == '__main__':
         info = bench.get_info(func_num)
         scale_range = [info['lower'], info['upper']]
         func = bench.get_function(func_num)
+
+        DG_groups, DG_cost = Comparison.DECC_DG(Dim, func)
+        VIL_groups, VIL_cost = Comparison.CCVIL(Dim, func)
+
         Max_iter = int(FEs / NIND / Dim)
+        DG_Max_iter = int((FEs - DG_cost) / NIND / Dim)
+        VIL_Max_iter = int((FEs - VIL_cost) / NIND / Dim)
         D_Max_iter = int((FEs - 30000) / NIND / Dim)
 
         VIL_obj_path = path.dirname(this_path) + "/Data/obj/VIL/f" + str(func_num)
@@ -60,24 +66,30 @@ if __name__ == '__main__':
             D_groups = Comparison.DECC_D(Dim, func, scale_range, groups_num=10, max_number=100)
             D_obj_trace = DE.CC(Dim, NIND, D_Max_iter, func, scale_range, D_groups)
             write_obj(D_obj_trace, D_obj_path)
+            print("    D finish !")
+
+            G_obj_trace = DE.GCC(Dim, NIND, Max_iter, func, scale_range)
+            write_obj(G_obj_trace, G_obj_path)
+            print("    G finish !")
+
+            MLCC_obj_trace = DE.MLCC(Dim, NIND, Max_iter, func, scale_range)
+            write_obj(MLCC_obj_trace, MLCC_obj_path)
+            print("    MLCC finish !")
+
+
+            VIL_obj_trace = DE.CC(Dim, NIND, VIL_Max_iter, func, scale_range, VIL_groups)
+            write_obj(VIL_obj_trace, VIL_obj_path)
+            print("    VIL finish !")
+
+
+            DG_obj_trace = DE.CC(Dim, NIND, DG_Max_iter, func, scale_range, DG_groups)
+            write_obj(DG_obj_trace, DG_obj_path)
+            print("    DG finish !")
 
             P1_groups = Proposal.autoRandom(Dim)
             P1_obj_trace = DE.CC(Dim, NIND, Max_iter, func, scale_range, P1_groups)
             write_obj(P1_obj_trace, P1_obj_path)
-
-            G_obj_trace = DE.GCC(Dim, NIND, Max_iter, func, scale_range)
-            write_obj(G_obj_trace, G_obj_path)
-
-            MLCC_obj_trace = DE.MLCC(Dim, NIND, Max_iter, func, scale_range)
-            write_obj(MLCC_obj_trace, MLCC_obj_path)
-
-            VIL_groups, VIL_cost = Comparison.CCVIL(Dim, func)
-            VIL_obj_trace = DE.CC(Dim, NIND, Max_iter, func, scale_range, VIL_groups)
-            write_obj(VIL_obj_trace, VIL_obj_path)
-
-            DG_groups, DG_cost = Comparison.DECC_DG(Dim, func)
-            DG_obj_trace = DE.CC(Dim, NIND, Max_iter, func, scale_range, DG_groups)
-            write_obj(DG_obj_trace, DG_obj_path)
+            print("    Proposal finish !")
 
 
             # """Ultimate Proposal: auto random grouping + Gaussian Sampling"""
